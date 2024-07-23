f051cf9263695d51df56d6e11b845a8ac5e06cc0
Eleonor Silva <eleonorsilva@gmail.com>
2024-07-23 18:56:57 +0100

    adjust card height depending on the photo height

--- getData.js
+++ getData.js
@@ -4,7 +4,7 @@
 
 async function getData() {
   const url =
-    "https://script.google.com/macros/s/AKfycbzrHleWeXi1qYJdxfynKObjm-aEyS3qvhEBQv80jmE2_olaA34bRqkIWHbKb9et1JC6/exec";
+    "https://script.google.com/macros/s/AKfycbxeoVBhb2Phi413aYUSdJC0iuQLM6I0e5EzAQeuusO_rcsP2cuYq9vWP6kiA6I3UkUa/exec";
   try {
     const response = await fetch(url);
     if (!response.ok) {
@@ -15,7 +15,6 @@ async function getData() {
     const data = json.data;
 
     return data;
-
   } catch (error) {
     console.error(error.message);
   }
@@ -23,13 +22,77 @@ async function getData() {
 
 getData()
   .then((data) => {
-    data.forEach((item) => {
-      const img = document.createElement("img");
-      img.src = `${item.url}`;
-    //   document.body.appendChild(img);
+    
+    let delayCounter = 0.2;
+
+    data.forEach((imgs, index) => {
+      // console.log(imgs);
+      
+      if (imgs.url) {
+
+        console.log(delayCounter);
+      
+          // delayCounter+=0.2;
+    
+        // delayCounter+=0.2;
+        
+        const img = document.createElement("img");
+        img.src = `${imgs.url}`;
+        // img.setAttribute("class", "img-card");
+        
+        const divImg = document.createElement("div");
+        divImg.setAttribute("class", "card");
+        divImg.setAttribute("id", `card-${index}`);
+        divImg.appendChild(img);
+        
+        if (imgs.height) {
+          console.log("vertical image!");
+          divImg.setAttribute("class", "card card-vertical");
+        }
+        
+        const cardsInner = document.getElementById("cards-inner");
+        cardsInner.appendChild(divImg);
+        
+        gsap.fromTo(`#card-${index}`, { opacity: 0, y: 70 }, { opacity: 1, y: 0, ease: "power1.out", duration: 0.5, delay: delayCounter});
+            delayCounter+=0.1;
+         
+        // gsap.fromTo(`#card-0`, { opacity: 0, y: 50, duration: 0.5 }, { opacity: 1, y: -5, duration: 1, delay: 0.2});
+        // gsap.fromTo(`#card-1`, { opacity: 0, y: 50, duration: 0.5 }, { opacity: 1, y: -5, duration: 1, delay: 0.3});
+        // gsap.fromTo(`#card-2`, { opacity: 0, y: 50, duration: 0.5 }, { opacity: 1, y: -5, duration: 1, delay: 0.4});
+        // gsap.fromTo(`#card-3`, { opacity: 0, y: 50, duration: 0.5 }, { opacity: 1, y: -5, duration: 1, delay: 0.5});
+        // console.log("loop");
+          
+          // tl.to(`#card-1`, {x: 100, ease: "power2.out",});
+          // tl.to(`#card-2`, {x: 100, ease: "power2.out",});
+
+          // gsap.utils.toArray("#card-").forEach((card, i) => {
+  
+          //   console.log(card)
+          //   // gsap.to(section.querySelector(".heading"), {
+          //   //   opacity: 1,
+          //   //   scrollTrigger: {
+          //   //     trigger: section,
+          //   //     pinnedContainer: "main",
+          //   //     pin: "main",
+          //   //     start: "center center",
+          //   //     end: "+=300",
+          //   //     toggleActions: "play none none reverse"
+          //   //   }
+          //   // });
+            
+          // });
+      }
     });
   })
   .catch((err) => {
     // Deal with the fact the chain failed
     console.error(err);
   });
+
+// window.addEventListener("load", (event) => {
+//   console.log("loaded!");
+// const cardsNumber = document.getElementsByClassName("card").length;
+
+// const tl = gsap.timeline();
+// tl.to(".card", {x: 100, ease: "power2.out"});
+// });
